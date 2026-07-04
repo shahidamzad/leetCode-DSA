c4bf60c1288a88fb4e1b5a36063c5ddc983b4d0c
Your Name <shahidamzad07@gmail.com>
2026-07-04 15:02:03 +0530

    Array

--- Arrays/index.js
+++ Arrays/index.js
@@ -8,13 +8,13 @@ let Discounts  = [5, 10 , 15 ,20] ;
 
 console.log(Discounts.length); // to find array of length 
 
-// Discounts.push(25) //  Using this method for adding numbers in Array
+Discounts.push(25) //  Using this method for adding numbers in Array
 
-// Discounts.pop(2) // Using this method for deleting or remove the last number in Array 
+Discounts.pop(2) // Using this method for deleting or remove the last number in Array 
 
-// Discounts.shift() // remove first element of the array 
+Discounts.shift() // remove first element of the array 
 
-// Discounts.unshift(30) // Add new element into the starting Of the Array 
+Discounts.unshift(30) // Add new element into the starting Of the Array 
 
 console.log(Discounts);
 
