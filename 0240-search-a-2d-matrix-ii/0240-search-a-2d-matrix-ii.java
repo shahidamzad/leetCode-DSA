7f2ecc091a460d81b173d4c87d336e2329e499c8
shahid hussain <151863732+shahidamzad@users.noreply.github.com>
2026-07-26 12:35:42 +0530

    Time: 3 ms (88.92%), Space: 48.3 MB (45.45%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -3,11 +3,18 @@ class Solution {
         int m = arr.length ;
         int n = arr[0].length;
 
-        for(int i = 0 ; i < m ; i++){
-            for(int j = 0 ; j < n ; j++){
-                if(arr[i][j]== target) return true;
-            }
+        int row = 0;
+        int cols = n-1 ;
+
+        while(row < m && cols >= 0){
+            if(arr[row][cols]==target){
+                return true;
+            }else if(arr[row][cols] > target){
+                cols--;
+            }else row++;
         }
+
+        
         return false;
     }
 }
