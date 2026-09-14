8ba0acb561ff53000589c0ed89e568e4b216f8db
Shahid Hussain <151863732+shahidamzad@users.noreply.github.com>
2026-09-14 11:15:53 +0530

    Time: 37 ms (26.77%), Space: 46.6 MB (88.9%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -5,11 +5,8 @@ class Solution {
 
         for (char c : s.toCharArray()) {
 
-            if (!st.isEmpty() && st.peek() == c) {
-                st.pop();
-            } else {
-                st.push(c);
-            }
+            if (!st.isEmpty() && st.peek() == c)  st.pop();
+                 else st.push(c);
         }
 
         StringBuilder sb = new StringBuilder();
